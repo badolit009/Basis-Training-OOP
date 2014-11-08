7d67aaff3b8ccdd2c315d10a83dd8e47e7c46663
badolit009 <badolit009@gmail.com>
2014-11-09 02:41:24 +0600

    ManyToManyRelationship Without Database Initially Uploaded

---
+++ ManyToManyRelationship/ManyToManyRelationship/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyToManyRelationship
+{
+    public class Enrollment
+    {
+        public Student AStudent { set; get; }
+        public Course CourseTaken { set; get; }
+
+        public DateTime EnrolledDate { set; get; }
+
+        public Enrollment(Student aStudent, Course courseTaken, DateTime enrolledDate)
+        {
+            AStudent = aStudent;
+            CourseTaken = courseTaken;
+            EnrolledDate = enrolledDate;
+        }
+    }
+}
---
+++ ManyToManyRelationship/ManyToManyRelationship/EnrollmentUI.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManyToManyRelationship
+{
+    public partial class EnrollmentUI : Form
+    {
+        private List<Enrollment> enrollments;
+        public EnrollmentUI(List<Student> students, List<Course> courses, List<Enrollment> enrollments )
+        {
+            InitializeComponent();
+            PopulateStudentComboBox(students);
+            PopulateCourseComboBox(courses);
+            this.enrollments = enrollments;
+            ShowListView();
+        }
+
+        private void ShowListView()
+        {
+            foreach (Enrollment enrollment in enrollments)
+            {
+                ListViewItem item = new ListViewItem(enrollment.AStudent.ToString());
+                item.SubItems.Add(enrollment.CourseTaken.Title);
+                item.SubItems.Add(enrollment.EnrolledDate.ToString());
+                EnrollListView.Items.Add(item);
+            }
+        }
+
+        private void PopulateCourseComboBox(List<Course> courses)
+        {
+            courseComboBox.DisplayMember="Title";
+            courseComboBox.DataSource = courses;
+            courseComboBox.ValueMember = "Code";
+        }
+
+        private void PopulateStudentComboBox(List<Student> students)
+        {
+            studentComboBox.DisplayMember = "RegNo";
+            studentComboBox.DataSource = students;
+            studentComboBox.ValueMember = "Name";
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            Enrollment enrollment = new Enrollment((Student)studentComboBox.SelectedItem,(Course)courseComboBox.SelectedItem,enrollmentDateTimePicker.Value);
+            enrollments.Add(enrollment);
+            EnrollListView.Items.Clear();
+            ShowListView();
+        }
+
+    }
+}
