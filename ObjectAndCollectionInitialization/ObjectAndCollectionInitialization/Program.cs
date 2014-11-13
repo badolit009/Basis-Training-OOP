cc248b87039ad24dea0086f2b4c08920aa8190cf
badolit009 <badolit009@gmail.com>
2014-11-13 11:09:51 +0600

    objectAndCollection Initialization Uploaded

---
+++ ObjectAndCollectionInitialization/ObjectAndCollectionInitialization/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAndCollectionInitialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student aStudent = new Student() {Name = "Badol", RegNo = "001"};
+            Student aStudent1 = new Student() {Name = "Labib", RegNo = "002"};
+
+
+            List<Student>StudentList=new List<Student>()
+            {
+                new Student(){Name="Badol",RegNo = "002"},
+                new Student(){Name = "Labib",RegNo = "003"}
+            };
+            foreach (var student in StudentList)
+            {
+                Console.WriteLine(student.Name+"   "+student.RegNo);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ObjectAndCollectionInitialization/ObjectAndCollectionInitialization/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAndCollectionInitialization
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string RegNo { get; set; }
+    }
+}
