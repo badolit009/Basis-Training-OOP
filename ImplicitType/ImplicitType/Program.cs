2b75ad98ec1ab48f776293a0d888da690e03d1f9
badolit009 <badolit009@gmail.com>
2014-11-13 11:05:18 +0600

    ImplicitType Initially Uploaded

---
+++ ImplicitType/ImplicitType/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplicitType
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var i = 50;
+            var s = "Rocky";
+            var d = 1.0;
+
+            var numbers = new[] {10, 30, 60};
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadKey();
+
+        }
+        
+    }
+}
